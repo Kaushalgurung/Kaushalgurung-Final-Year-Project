95a4dc2a711ad4f29a0099fc50d2d4ba8651e33d
Kaushalgurung <kaushalgurung2057@gmail.com>
2022-02-10 10:53:25 +0545

    mongoDB connection

--- Shopping-api/index.js
+++ Shopping-api/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute
 
-app.listen(5000, () => {
-    console.log("Backend server is running")
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+    )
+.then(() => console.log("Database connected."))
+.catch((err) => { console.log(err);
+});
+
+app.get("/api/test", () => {
+    console.log("test sucessfull");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running.")
 });
