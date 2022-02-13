39477f5473cde61c998f1f00ceaeaf477c2b016f
Kaushalgurung <kaushalgurung2057@gmail.com>
2022-02-13 13:00:37 +0545

    user register

--- Shopping-api/index.js
+++ Shopping-api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+
 dotenv.config();
 
 mongoose
--- Shopping-api/routes/auth.js
+++ Shopping-api/routes/auth.js
@@ -1,19 +1,39 @@
 const req = require("express/lib/request");
-const USer = require("../models/User");
+const User = require("../models/User");
 const router = require("express").Router();
+const Cryptojs = require("crypto-js");
 
 //register
 
-router.post("/rehister", async(req,res)=>{
+router.post("/register", async(req,res)=>{
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password:req.body.password,
+        password:Cryptojs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     });
 
     try{const savedUser = await newUser.save();
         res.status(201).json(savedUser);
     console.log(savedUser);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//login
+
+router.post("/login", async(req,res)=>{
+    try{
+        const user = await User.findOne ({username: req.body.username});
+        !user && res.status(401).json("no user found");
+
+        const hashedPassword = Cryptojs.AES.decrypt(user.password, process.env.PASS_SEC);
+        const password = hashedPassword.toString(Cryptojs.enc.Utf8);
+
+        password !==req.body.password && res.status(401).json("incorrect password");
+
+        res.status(200).json(user);
     }
     catch(err){
         res.status(500).json(err);
