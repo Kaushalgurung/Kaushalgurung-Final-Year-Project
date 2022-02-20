2f0ef4cd1f6e3cd959b7c8a46ce1f36bc0089fe0
Kaushalgurung <kaushalgurung2057@gmail.com>
2022-02-20 16:24:06 +0545

    User update and login error fix

--- Shopping-api/routes/auth.js
+++ Shopping-api/routes/auth.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 
 //register
 
-router.post("/register", async(req,res)=>{
+router.post("/register", async (req,res )=> {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
@@ -15,7 +15,7 @@ router.post("/register", async(req,res)=>{
 
     try{const savedUser = await newUser.save();
         res.status(201).json(savedUser);
-    console.log(savedUser);
+        console.log(savedUser);
     }
     catch(err){
         res.status(500).json(err);
@@ -23,46 +23,32 @@ router.post("/register", async(req,res)=>{
 });
 
 //login
+router.post("/login", async (req,res)=>{
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("No user name! or Incorrect Username!");
 
-router.post('/login', async (req, res) => {
-    try{
-        const user = await User.findOne(
-            {
-                userName: req.body.user_name
-            }
-        );
+        const hashedPassword = Cryptojs.AES.decrypt(
+            user.password, 
+            process.env.PASS_SEC);
 
-        !user && res.status(401).json("Incorrect User-name");
-
-        const hashedPassword = CryptoJS.AES.decrypt(
-            user.password,
-            process.env.PASS_SEC
-        );
-
-
-        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-        const inputPassword = req.body.password;
-        
-        originalPassword != inputPassword && 
-            res.status(401).json("Incorrect Password");
+        const Orginalpassword = hashedPassword.toString(Cryptojs.enc.Utf8);
+        Orginalpassword !==req.body.password && res.status(401).json("Incorrect Password!");
 
         const accessToken = jwt.sign(
-        {
-            id: user._id,
-            isAdmin: user.isAdmin,
-        },
-        process.env.JWT_SEC,
-            {expiresIn:"3d"}
-        );
-  
-        const { password, ...others } = user._doc;  
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SEC,
+                {expiresIn:"7d"}
+            );
+        
+        const { password, ...others } = user._doc;
+
         res.status(200).json({...others, accessToken});
-
     }catch(err){
-        res.status(500).json(err);
+        res.status(500).json(err);   
     }
-
 });
-
 module.exports = router;
